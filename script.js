bd367a718ae395eb0f6de5bfa5c102ffb54d2406
MudasirSherwani <mudasir9757@gmail.com>
2022-09-19 19:33:29 +0500

    Add bookID as Random function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 function BookStoring(title, author) {
-  const bookFields = { StoredTitle: title, StoredAuthor: author};
+  var bookID = Math.floor(Math.random() * 100);
+  const bookFields = { StoredID: bookID, StoredTitle: title, StoredAuthor: author};
   localStorage.setItem('Books_Storage_Data', JSON.stringify(bookFields));
   return true;
 }
