921206799dde203ac525ac11e9ef3791c8f02803
MudasirSherwani <mudasir9757@gmail.com>
2022-09-19 19:00:44 +0500

    Define BookStoring Function and Call from submit

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-/*Variables*/
 
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const button = document.querySelector('.btn');
-const bookList = document.querySelector('#book-list');
-let book;
-
-const  addBook = [ {
-  title: titleInput
-  author: authorInput
-}];
-
-function bookStore (){
-  localStorage.setItem('booksData', JSON.stringify(addBook))
-} 
-
-button.addEventListener('click',(e) =>{
-  e.preventDefault();
-  bookStore();
-  console.log(addBook.title);
-});
+function BookStoring(title, author) {
+  const bookFields = { StoredTitle: title, StoredAuthor: author};
+  localStorage.setItem('Books_Storage_Data', JSON.stringify(bookFields));
+  return true;
+}
