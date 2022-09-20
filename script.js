2978f7129fb6ed103b3e35a926c6063fec6ed9bf
MudasirSherwani <mudasir9757@gmail.com>
2022-09-20 20:20:17 +0500

    Book Class Added with some methods.

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
+const titleNew = document.querySelector('#title');
+const authorNew = document.querySelector('#author');
 const form = document.querySelector('#book-form');
 const collection = document.querySelector('#collection');
-let book;
+let book = [];
 let bookList = JSON.parse(localStorage.getItem('bookList')) || [];
-function addBook() {
-  book = {
-    title: title.value,
-    author: author.value,
-    id: Math.floor(Math.random() * 100000),
-  };
-  bookList.push(book);
-  localStorage.setItem('bookList', JSON.stringify(bookList));
+
+class bookClass {
+  constructor (id, title, author){
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+
+  addBook(){
+    book = {id: this.id, title: this.title, author: this.author };
+    bookList.push(book);
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+  }
+
+  removeBook(bid){
+    bookList = bookList.filter((books) => books.id !== bid);
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+  }
+
 }
-function removeBook(id) {
-  bookList = bookList.filter((books) => books.id !== id);
-  localStorage.setItem('bookList', JSON.stringify(bookList));
-}
+
 function populate(book) {
   const row = document.createElement('tr');
   const bookTitle = document.createElement('td');
@@ -29,15 +37,18 @@ function populate(book) {
   collection.append(row);
   removeBtn.addEventListener('click', () => {
     removeBtn.parentElement.remove();
-    removeBook(book.id);
+    let objBookClass = new bookClass();
+    objBookClass.removeBook(book.id);
   });
 }
-bookList.forEach(populate);
 
+bookList.forEach(populate);
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (title.value !== '' && author.value !== '') {
-    addBook();
+  if (titleNew.value !== '' && authorNew.value !== '') {
+    let bookId=Math.floor(Math.random() * 100000);
+    let objBookClass = new bookClass(bookId,titleNew.value,authorNew.value)
+    objBookClass.addBook();
     populate(book);
     form.reset();
   } else {
