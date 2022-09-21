b1d2544eb6d2f1ff09b24b7c6adbbbbb8379c980
MudasirSherwani <mudasir9757@gmail.com>
2022-09-21 17:22:26 +0500

    Changes Done Suggested By Code Reviewe PR#3

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
+const titleNew = document.querySelector('#title');
+const authorNew = document.querySelector('#author');
 const form = document.querySelector('#book-form');
 const collection = document.querySelector('#collection');
-let book;
+let book = [];
 let bookList = JSON.parse(localStorage.getItem('bookList')) || [];
-function addBook() {
-  book = {
-    title: title.value,
-    author: author.value,
-    id: Math.floor(Math.random() * 100000),
-  };
-  bookList.push(book);
-  localStorage.setItem('bookList', JSON.stringify(bookList));
+
+class BookClass {
+  constructor(id, title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+
+  addBook() {
+    book = { id: this.id, title: this.title, author: this.author };
+    bookList.push(book);
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+  }
+
+  removeBook() {
+    bookList = bookList.filter((books) => books.id !== this.id);
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+  }
 }
-function removeBook(id) {
-  bookList = bookList.filter((books) => books.id !== id);
-  localStorage.setItem('bookList', JSON.stringify(bookList));
-}
+
 function populate(book) {
   const row = document.createElement('tr');
   const bookTitle = document.createElement('td');
@@ -28,20 +35,22 @@ function populate(book) {
   collection.append(row);
   removeBtn.addEventListener('click', () => {
     removeBtn.parentElement.remove();
-    const objBookClassRemove = new BookClass(book.id,book.title, book.author);
+    const objBookClassRemove = new BookClass(book.id, book.title, book.author);
     objBookClassRemove.removeBook();
-
   });
 }
-bookList.forEach(populate);
 
+bookList.forEach(populate);
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (title.value !== '' && author.value !== '') {
-    addBook();
+  if (titleNew.value !== '' && authorNew.value !== '') {
+    const bookId = Math.floor(Math.random() * 100000);
+    const objBookClass = new BookClass(bookId, titleNew.value, authorNew.value);
+    objBookClass.addBook();
     populate(book);
     form.reset();
   } else {
+    // eslint-disable-next-line no-alert
     alert('Please enter a title and author');
   }
 });
